a56ca949d5263332b6848569ae5aec8dd00cfe66
Oleg Zhatkin <oa.zhatkin@cd-it.ru>
2024-12-22 14:27:52 +0300

    12

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [inputedText, setInputedText] = useState("");
+  const [displayList, setDisplayList] = useState([]);
+
+  const onButtonClick = () => {
+    setDisplayList((prev) => [...prev, inputedText]);
+    setInputedText("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.layout}>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="input text"
+          style={styles.input}
+          onChangeText={(text) => {
+            setInputedText(text);
+          }}
+          value={inputedText}
+        />
+        <View>
+          <TouchableOpacity style={styles.button} onPress={onButtonClick}>
+            <Text>123</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.list}>
+        <FlatList data={displayList} renderItem={(listItem) => {
+          return (
+            <View style={styles.elem}>
+              <Text>{listItem.item}</Text>
+            </View>
+          )
+        }}/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    paddingVertical: 60,
+    paddingHorizontal: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    alignItems: "end",
+  },
+  button: {
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "red",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "blue",
+    width: "80%",
+    marginRight: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  list: {
+    flex: 10,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    borderTopWidth: 4,
+    borderTopColor: "green",
+    padding: 10,
+    gap: 10,
+  },
+  elem: {
+    height: 40,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 5,
+    borderWidth: 2,
+    borderColor: "grey",
+    borderRadius: 4,
   },
 });
